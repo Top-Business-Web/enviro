c7e7f6a9afd8c080b895de0a8338a946709b5d81
Abdullah <abdullahalhomsi520@gmail.com>
2023-04-07 19:26:54 +0200

    Create About Us

--- app/Http/Controllers/Admin/AboutUsController.php
+++ app/Http/Controllers/Admin/AboutUsController.php
@@ -3,14 +3,37 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\UpdateAboutUs;
 use App\Models\About;
+use App\Models\Setting;
+use App\Traits\PhotoTrait;
 
 class AboutUsController extends Controller
 {
-    public function index()
+    use PhotoTrait;
+
+    public function index(){
+        $about_us = About::first();
+        return view('admin.about_us.index',compact('about_us'));
+    }
+
+    public function update(UpdateAboutUs $request)
     {
-        $about_us = About::get()->first();
-        return $about_us;
-        return view('Admin.about_us.index', compact('about_us'));
+        $about_us = About::findOrFail($request->id);
+
+        $inputs = $request->all();
+
+        if ($request->has('image'))
+        {
+            if (file_exists(public_path('assets/uploads/admins/images/') .$about_us->image)) {
+                unlink(('assets/uploads/admins/images/') .$about_us->image);
+            }
+            $inputs['image'] =  $request->image != null ? $this->saveImage($request->image, 'assets/uploads/admins/images' , 'photo') : $inputs['image'];
+        }
+
+        if ($about_us->update($inputs))
+            return response()->json(['status' => 200]);
+        else
+            return response()->json(['status' => 405]);
     }
 }
